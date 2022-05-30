c0eb34c8e3a39c8a1b52a305aba649b03e4a4dc3
chrisC46 <christophe.cuaz@gmail.com>
2022-05-30 11:36:09 +0100

    add Button

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
       {/* comment dans XML*/}
       Hello World
+      <hr/>
+    <Button/>
     </div>
+
   );
 }
 

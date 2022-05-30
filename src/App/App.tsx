f25a5bf92cada9c81fcc22b627dc9f703fdbd3a0
chrisC46 <christophe.cuaz@gmail.com>
2022-05-30 14:52:22 +0100

    ajout du css

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -14,12 +14,15 @@ function App() {
       <Button action={() => alert("Le bouton est clické")}>
         <div>Hello</div>
       </Button>
+
       <Button action={() => alert("Le bouton est clické")}>
         <div>Hello</div>
         <hr />
         <div>De Lu</div>
       </Button>
+
       <Button action={() => alert("Le bouton est clické")}>Hello String</Button>
+      
       <Button>With Child</Button>
     </div>
   );

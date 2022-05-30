fd60c587b80ad9c371fa6d46c372001f82ca7992
chrisC46 <christophe.cuaz@gmail.com>
2022-05-30 11:03:35 +0100

    add app folder and clean appcss

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* comment dans XML*/}
+      Hello World
+    </div>
+  );
+}
+
+export default App;
